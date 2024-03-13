8581b537407e3818f6b229970cdefa88c307ae83
DJK101 <danieljeffersonkennedy@gmail.com>
2024-03-13 17:53:47 +0000

    Add Toast Details to attributes

--- src/Types.tsx
+++ src/Types.tsx
@@ -1,8 +1,22 @@
-
 interface User {
   name: string;
   email: string;
   password: string;
   dob: string;
   admin: boolean;
+}
+
+interface ToastDetails {
+  title: string;
+  content: string;
+  style:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "danger"
+    | "warning"
+    | "info"
+    | "dark"
+    | "light"
+    | "";
 }
