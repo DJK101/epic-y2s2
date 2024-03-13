6479c7370dcce67088f4a9f9fa052483d7e2c6b8
DJK101 <danieljeffersonkennedy@gmail.com>
2024-03-13 15:09:25 +0000

    Update SignUp.tsx to use localStorage to save users in array of users

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,69 +1,155 @@
 import { useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, Toast } from "react-bootstrap";
 import "react-datetime/css/react-datetime.css";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [newUser, setNewUser] = useState<User>({
+    name: "",
+    email: "",
+    password: "",
+    dob: "",
+    admin: false,
+  });
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [dob, setDob] = useState("");
   const [agreedToTerms, setAgreedToTerms] = useState(false);
+  const [showToast, setShowToast] = useState(false);
+  const [toastTitle, setToastTitle] = useState("");
+  const [toastContent, setToastContent] = useState("");
+  const [toastStyle, setToastStyle] = useState("");
+
+  const clearForm = () => {
+    setNewUser({
+      name: "",
+      email: "",
+      password: "",
+      dob: "",
+      admin: false,
+    });
+    setConfirmPassword("");
+    setAgreedToTerms(false);
+  };
+
+  const addUser = () => {
+    const newUserr: User = newUser;
+
+    const storedUsers = localStorage.getItem("users");
+    const users: [User] = storedUsers ? JSON.parse(storedUsers) : [];
+
+    users.push(newUserr);
+
+    localStorage.setItem("users", JSON.stringify(users));
+  };
+
+  const userExists = () => {
+    const storedUsers = localStorage.getItem("users");
+    const users: [User] = storedUsers ? JSON.parse(storedUsers) : [];
+
+    return users.some((user) => user.email === newUser.email);
+  };
 
   const handleSubmit = (event: any) => {
     event.preventDefault();
 
-    // Check if all fields are filled
-    if (!email || !password || !confirmPassword || !dob || !agreedToTerms) {
-      alert("Please fill in all fields.");
+    if (newUser.password !== confirmPassword) {
+      setToastTitle("An Error Occured!");
+      setToastContent("Passwords do not match");
+      setToastStyle("danger");
+      setShowToast(true);
       return;
     }
 
-    // Check if passwords match
-    if (password !== confirmPassword) {
-      alert("Passwords do not match.");
+    if (userExists()) {
+      setToastTitle("An Error Occured!");
+      setToastContent(
+        "Account with email '" + newUser.email + "' already exists"
+      );
+      setToastStyle("danger");
+      setShowToast(true);
       return;
     }
 
-    // Store user data in local storage
-    localStorage.setItem(email, password);
+    addUser();
 
-    // Optionally, redirect to a success page or perform other actions
-    alert("User registered successfully!");
+    setToastTitle("Success!");
+    setToastContent("User " + newUser.email + " registered successfully!");
+    setToastStyle("success");
+    setShowToast(true);
+    clearForm();
+  };
+
+  const handleChange = (e: any, key: string) => {
+    const { value, type, checked } = e.target;
+    const newValue = type === "checkbox" ? checked : value;
+    setNewUser((prevState) => ({
+      ...prevState,
+      [key]: newValue,
+    }));
   };
 
   return (
-    <Container className="d-flex justify-content-center align-items-center pt-5">
+    <Container className="d-flex flex-column justify-content-center align-items-center pt-5">
+      <Toast
+        onClose={() => setShowToast(false)}
+        show={showToast}
+        delay={1200}
+        autohide
+        bg={toastStyle}
+      >
+        <Toast.Header>
+          <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+          <strong className="me-auto">{toastTitle}</strong>
+        </Toast.Header>
+        <Toast.Body>{toastContent}</Toast.Body>
+      </Toast>
+
+      <h2>Sign Up</h2>
       <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Full Name"
+            size="lg"
+            value={newUser.name}
+            onChange={(e) => handleChange(e, "name")}
+            required
+          />
+        </Form.Group>
+
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email address</Form.Label>
           <Form.Control
             type="email"
             placeholder="Enter email"
             size="lg"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={newUser.email}
+            onChange={(e) => handleChange(e, "email")}
+            required
           />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="password">
+        <Form.Group className="mb-3" controlId="pwd">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             placeholder="Password"
             size="lg"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={newUser.password}
+            onChange={(e) => handleChange(e, "password")}
+            required
           />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="password">
+        <Form.Group className="mb-3" controlId="pwd-confirm">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
             type="password"
-            placeholder="Password"
+            placeholder="Re-enter Password"
             size="lg"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
+            required
           />
         </Form.Group>
 
@@ -73,8 +159,9 @@ export default function SignUp() {
             type="date"
             placeholder="Date of Birth"
             size="lg"
-            value={dob}
-            onChange={(e) => setDob(e.target.value)}
+            value={newUser.dob}
+            onChange={(e) => handleChange(e, "dob")}
+            required
           />
         </Form.Group>
 
@@ -91,12 +178,17 @@ export default function SignUp() {
             }
             checked={agreedToTerms}
             onChange={(e) => setAgreedToTerms(e.target.checked)}
+            required
           />
         </Form.Group>
 
         <Button variant="primary" type="submit">
           Submit
         </Button>
+
+        <p className="mt-3">
+          Already have an account? <Link to="/signup">Sign in</Link>
+        </p>
       </Form>
     </Container>
   );
