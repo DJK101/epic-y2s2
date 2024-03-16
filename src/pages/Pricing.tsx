6ea0e0c2241068369869ae2bc1bde28237b97a23
DJK101 <danieljeffersonkennedy@gmail.com>
2024-03-16 02:57:15 +0000

    Add Pricing Page

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,10 +1,39 @@
-import { Container, Row } from "react-bootstrap";
+import { Button, Card, Container, Row } from "react-bootstrap";
 
 export default function Pricing() {
   return (
     <Container>
       <Row>
-        <h1>Pricing Text</h1>
+        <h1>Current Offers</h1>
+      </Row>
+      <Row className="d-flex justify-content-around mt-5">
+          <Card className="w-25 p-0">
+            <Card.Img variant="top" src="./src/assets/game-controller.jpg"/>
+            <Card.Body>
+              <Card.Title>Single Console</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="d-flex justify-content-center">
+              <Button variant="warning">Buy Now</Button>
+            </Card.Footer>
+          </Card>
+          <Card className="w-25 p-0">
+            <Card.Img variant="top" src="./src/assets/game-controller-combo.jpg"/>
+            <Card.Body>
+              <Card.Title>Friendship Deal</Card.Title>
+              <Card.Text>
+                This card has supporting text below as a natural lead-in to
+                additional content.{" "}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="d-flex justify-content-center">
+              <Button variant="warning">Buy Now</Button>
+            </Card.Footer>
+          </Card>
       </Row>
     </Container>
   );
