81c31be37845b8cc2a6443d6f84ab74cb7513185
DJK101 <danieljeffersonkennedy@gmail.com>
2024-03-16 03:38:12 +0000

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <NavBar />
+    <div className="d-flex flex-column justify-content-between vh-100">
+      <div>
+        <NavBar />
+      </div>
+
+      <p className="bg-body-tertiary text-center mt-5 mb-0">
+        &copy; 2024 Arena Arcade
+      </p>
     </div>
   );
 }
