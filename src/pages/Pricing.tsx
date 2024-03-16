022410477a701110893c26f54ddcfc7df1e8b972
DJK101 <danieljeffersonkennedy@gmail.com>
2024-03-16 02:57:38 +0000

    Update Pricing Page

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 export default function Pricing() {
   return (
@@ -6,34 +6,42 @@ export default function Pricing() {
       <Row>
         <h1>Current Offers</h1>
       </Row>
-      <Row className="d-flex justify-content-around mt-5">
-          <Card className="w-25 p-0">
-            <Card.Img variant="top" src="./src/assets/game-controller.jpg"/>
+      <Row className="d-flex justify-content-around mt-4" xs={1} sm={1} md={2}>
+        <Col>
+          <Card className="p-0" border="secondary">
+            <Card.Img variant="top" src="./src/assets/game-controller.jpg" />
+            <Card.Header>Single Console</Card.Header>
             <Card.Body>
-              <Card.Title>Single Console</Card.Title>
+              <Card.Title>1 Console for $40</Card.Title>
               <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+                Made with high quality materials. This console is bound to
+                provide hours of fun, get it for a very afforable price today!
               </Card.Text>
             </Card.Body>
             <Card.Footer className="d-flex justify-content-center">
               <Button variant="warning">Buy Now</Button>
             </Card.Footer>
           </Card>
-          <Card className="w-25 p-0">
-            <Card.Img variant="top" src="./src/assets/game-controller-combo.jpg"/>
+        </Col>
+        <Col>
+          <Card className="p-0" border="secondary">
+            <Card.Img
+              variant="top"
+              src="./src/assets/game-controller-combo.jpg"
+            />
+            <Card.Header>Friendship Deal</Card.Header>
             <Card.Body>
-              <Card.Title>Friendship Deal</Card.Title>
+              <Card.Title>2 Consoles for $70</Card.Title>
               <Card.Text>
-                This card has supporting text below as a natural lead-in to
-                additional content.{" "}
+                Competing with your friends is at the forefront of this console,
+                include them in the fun with this deal!
               </Card.Text>
             </Card.Body>
             <Card.Footer className="d-flex justify-content-center">
               <Button variant="warning">Buy Now</Button>
             </Card.Footer>
           </Card>
+        </Col>
       </Row>
     </Container>
   );
